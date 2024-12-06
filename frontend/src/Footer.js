063e398de989a757bde21fd591e6c17a220bb0ff
njb2163 <njb2163@columbia.edu>
2024-12-06 18:46:39 -0500

    nick changes

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,26 +1,16 @@
-
-
-
 import './App.css';
 
 function Footer() {
     return (
         <footer>
             <div className="group-2">
-                <img className="vuesax-outline-bag" src={require("./images/profile.png")} />
-                <img className="vuesax-outline" src={require("./images/quests.png")}/>
-                <div className="frame-2">
-                    <div className="overlap-group-wrapper">
-                        <div className="overlap-group">
-                            <img className="vector" src="img/vector.svg" />
-                            <div className="rectangle-2"></div>
-                            <img className="vector-2" src="img/image.svg" />
-                            <img className="vector-3" src="img/vector-2.svg" />
-                        </div>
-                    </div>
-                    <div className="rectangle-3"></div>
-                </div>
-                <img className="vuesax-outline-2" src={require("./images/messages.png")} />
+                <img className="footer-profile" src={require("./images/profile.png")} />
+                <img className="footer-quests" src={require("./images/quests.png")}/>
+                <img 
+                    className="footer-settings" 
+                    src={require("./images/settings.png")} 
+                />
+                <img className="footer-messages" src={require("./images/messages.png")} />
             </div>
         </footer>
     );
--- frontend/src/Personality.js
+++ frontend/src/Personality.js
@@ -6,7 +6,7 @@ function Personality({ user }) {
   return (
     <div>
       <div className="overlap">
-        <img className="img" src="img/rectangle-131.svg" />
+        <img className="profile-background-img" src={require(`./${"images/Victoria-background.png"}`)} />
         <div className="group">
           <div className="text-wrapper">{user.name}</div>
           <div className="text-wrapper-2">{user.type}</div>
@@ -15,32 +15,44 @@ function Personality({ user }) {
       </div>
       <div className="overlap-2">
         <div className="text-wrapper-4">Background</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Background-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.background}%</div>
       </div>
       <div className="overlap-3">
         <div className="text-wrapper-6">Interests</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Interests-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.interests}%</div>
       </div>
       <div className="overlap-4">
         <div className="text-wrapper-4">Preferences</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Preferences-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.preferences}%</div>
       </div>
       <div className="overlap-5">
         <div className="text-wrapper-7">Values</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Values-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.values}%</div>
       </div>
       <div className="overlap-6">
         <div className="text-wrapper-8">Traits</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Traits-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.traits}%</div>
       </div>
       <div className="overlap-7">
         <div className="text-wrapper-9">Perspectives</div>
-        <div className="ellipse"></div>
+        <div className="ellipse">
+          <img src={require(`./${"images/Perspectives-thumbnail.png"}`)} />
+        </div>
         <div className="text-wrapper-5">{user.sections.perspectives}%</div>
       </div>
     </div>
